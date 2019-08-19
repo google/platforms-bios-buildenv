7dceeb9ddc6e51b91bf3911b838c1ffd01a52488
Luis Alberto Herrera <luisalberto@google.com>
2019-08-19 10:23:27 -0700

    Initial script to generate a debian root file system.
    This is script will generate a deterministic Debian filesystem. It will
    be imported and used as part of a Docker container.
    
    Change-Id: If1f607112f50649d003f7644e7707695a28b2c2e
    
    Former-commit-id: 8e821b1a73384e5ecbf814a8a206d0ccbbbaf289

---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# This generates a deterministic build environment.
+# It is based on the Debian distribution and the debuerreotype project.
+# Run this script as:
+# $ sudo ./build.sh
+# This will generate a rootfs.tar.xz that has an root image suitable to be used
+# on a Docker container. A rootfs.sha256 contains a SHA256 checksum of the
+# uncompressed and compressed images.
+#
+# When reviewing changes to this script, the reviewer must run the script
+# locally and verify that the output matches the output on the review.
+
+set -e
+set -x
+
+# Some configuration parameters.
+# Update package list bellow.
+DISTRO=buster
+TIMESTAMP=2019-08-01T00:00:00Z
+
+# Clean up previous artifacts.
+rm -rf rootfs
+rm -rf rootfs.sha256
+rm -rf rootfs.tar
+rm -rf rootfs.tar.xz
+rm -rf debuerreotype
+
+# Get a well know version of debuerreotype.
+# Changes on debuerreotype can lead to changes on the output.
+git clone --branch 0.10 --depth 1 https://github.com/debuerreotype/debuerreotype.git
+
+BIN=debuerreotype/scripts
+
+$BIN/debuerreotype-init rootfs $DISTRO 2019-08-01T00:00:00Z
+$BIN/debuerreotype-minimizing-config rootfs
+$BIN/debuerreotype-apt-get rootfs update -qq
+$BIN/debuerreotype-apt-get rootfs dist-upgrade -yqq
+
+# Update this list to add software to the build environment.
+
+$BIN/debuerreotype-apt-get rootfs install -yqq --no-install-recommends \
+  acpica-tools \
+  bc \
+  bison \
+  build-essential \
+  flex \
+  git \
+  nasm \
+  python-distutils-extra \
+  uuid-dev \
+
+$BIN/debuerreotype-debian-sources-list rootfs $DISTRO
+
+# Delete cached Python bytecode.
+find -type d -name  __pycache__ -prune  -exec rm -r "{}" \;
+
+$BIN/debuerreotype-tar rootfs rootfs.tar
+sha256sum rootfs.tar > rootfs.sha256
+xz rootfs.tar
+sha256sum rootfs.tar.xz >> rootfs.sha256
+
+chown --reference=build.sh rootfs.tar.xz rootfs.sha256
+
+rm -rf rootfs
+rm -rf debuerreotype
